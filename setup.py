f0180442c5972c6cf9d2a3133a5eb2829e177419
Jon Wayne Parrott <jonwayne@google.com>
2017-03-02 13:50:05 -0800

    Release v0.3.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ long_description = open('README.rst', 'r', encoding='utf-8').read()
 setup(
     name='flask-talisman',
 
-    version='0.2.1',
+    version='0.3.0',
 
     description='HTTP security headers for Flask.',
     long_description=long_description,
